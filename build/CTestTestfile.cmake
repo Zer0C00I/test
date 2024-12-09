70733b31077b8deafa0e3c718700adfcb0fc84e6
Zer0C00I <you@example.com>
2024-12-09 17:36:03 +0200

    first

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /media/anon/7A783A9D51AB320A/VideoCourse/С&C++/[OTUS] C++ Developer. Professional (2023)/01. Система сборки. build, test and deploy_/source
+# Build directory: /media/anon/7A783A9D51AB320A/VideoCourse/С&C++/[OTUS] C++ Developer. Professional (2023)/01. Система сборки. build, test and deploy_/source/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(test_version "test_version")
+set_tests_properties(test_version PROPERTIES  _BACKTRACE_TRIPLES "/media/anon/7A783A9D51AB320A/VideoCourse/С&C++/[OTUS] C++ Developer. Professional (2023)/01. Система сборки. build, test and deploy_/source/CMakeLists.txt;74;add_test;/media/anon/7A783A9D51AB320A/VideoCourse/С&C++/[OTUS] C++ Developer. Professional (2023)/01. Система сборки. build, test and deploy_/source/CMakeLists.txt;0;")
